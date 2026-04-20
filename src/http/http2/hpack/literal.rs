e3468a5387ec84d2eb99f6addad2e8f55199b8b2
Daniel Quednow <danquednow@gmail.com>
2026-04-20 19:57:25 -0400

    Update literal parsing and writing
    Have the read functions say how much data they parsed
    Added a string write function that doesn't do huffman encoding

--- src/http/http2/hpack/literal.rs
+++ src/http/http2/hpack/literal.rs
@@ -1,5 +1,7 @@
 use std::io::{self, Read, Write};
 
+use bstr::{BStr, BString};
+
 use crate::http::http2::hpack::huffman::decode_bytes;
 
 pub fn parse_integer(bits: u8, data: &[u8]) -> io::Result<(u64, usize)> {
@@ -22,7 +24,7 @@ pub fn parse_integer(bits: u8, data: &[u8]) -> io::Result<(u64, usize)> {
         offset += 1;
 
         if byte & 0x80 == 0 {
-            return Ok((output, offset - 1));
+            return Ok((output, offset));
         }
     }
 
@@ -34,12 +36,13 @@ pub fn write_integer(
     bits: u8,
     mut int: u64,
     writer: &mut impl Write,
-) -> io::Result<()> {
+) -> io::Result<usize> {
     let mask = (1 << bits) - 1;
+    let mut len = 1;
 
     if int < mask as u64 {
         let output = [(prefix << bits) | ((int as u8) & mask)];
-        return writer.write_all(&output);
+        return writer.write_all(&output).map(|_| len);
     }
 
     let mut output = [(prefix << bits) | mask];
@@ -51,19 +54,28 @@ pub fn write_integer(
         output[0] = ((int & 0x7F) as u8) | 0x80;
         writer.write_all(&output)?;
         int >>= 7;
+        len += 1;
     }
 
     output[0] = (int & 0x7F) as u8;
-    writer.write_all(&output)
+    writer.write_all(&output).map(|_| len)
 }
 
-pub fn parse_string(data: &[u8]) -> io::Result<(Vec<u8>, usize)> {
+pub fn parse_string(data: &[u8]) -> io::Result<(BString, usize)> {
     let (len, parsed) = parse_integer(7, data)?;
     let len = len as usize;
 
     if data[0] & 0x80 == 0x80 {
-        decode_bytes(len, &data[parsed..]).map(|v| (v, parsed + len))
+        decode_bytes(&data[parsed..parsed + len]).map(|v| (v, parsed + len))
     } else {
-        Ok((data[parsed..parsed + len].to_vec(), parsed + len))
+        Ok((data[parsed..parsed + len].to_vec().into(), parsed + len))
     }
+}
+
+pub fn write_string(s: &[u8], writer: &mut impl Write) -> io::Result<usize> {
+    let written = write_integer(0x0, 1, s.len() as u64, writer)?;
+
+    writer.write_all(s)?;
+
+    Ok(written + s.len())
 }
